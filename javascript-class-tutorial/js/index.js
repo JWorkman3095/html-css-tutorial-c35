3aae9d731ca495f105cd3fb732d6a958f59f3640
Jeff Workman <jworkhollr@aol.com>
2022-03-25 15:21:29 -0400

    added customer

--- javascript-class-tutorial/js/index.js
+++ javascript-class-tutorial/js/index.js
@@ -8,19 +8,48 @@ class Customer {
         this.name = name;
         this.sales = sales;
     } 
-
+    
     log() {
         console.log(this.id, this.name, this.sales);
     }
+    
 }
 
 let customers = [
     new Customer(1, "MAX", 1000),
     new Customer(2, "PG", 10000),
-    new Customer(3, "TREE", 8900),
-    new Customer(4, "Test")
-]
+    new Customer(3, "TREE", 8900)
+];
 
-for (let customer of customers) {
-    customer.log();
+const loaded = () => {
+    display (customers);
 }
+
+const display = (customers) => {
+
+    const tbody = document.getElementById("custs");
+
+    tbody.innerHTML = "";
+    for (let customer of customers) {
+        customer.log();
+        let tr = "<tr>";
+        tr +=`<td>${customer.id}</td>`;
+        tr +=`<td>${customer.name}</td>`;
+        tr +=`<td>${customer.sales}</td>`;
+        tr += "</tr>"
+        tbody.innerHTML += tr;
+    }
+}
+    const add = () => {
+        let inpIdCtrl = document.getElementById("pId");
+        let inpNameCtrl = document.getElementById("pName");
+        let inpSalesCtrl = document.getElementById("pSales");
+        let cust = new Customer();
+        cust.id = +inpIdCtrl.value;
+        cust.name = inpNameCtrl.value;
+        cust.sales = +inpSalesCtrl.value;
+        customers.push(cust);
+        display(customers);
+
+    }
+
